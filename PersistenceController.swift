f6f249dc0b539c361e91d9bfbd24e15eaf7afa2c
nnl3336 <may.be.be.k12@gmail.com>
2025-08-22 15:43:37 +0900

    commit-⭐️8.22。o UICollectionView

---
+++ PersistenceController.swift
@@ -0,0 +1,29 @@
+//
+//  PersistenceController.swift
+//  GallerySelectUITests
+//
+//  Created by Yuki Sasaki on 2025/08/22.
+//
+
+import SwiftUI
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "ModelName") // あなたの Core Data モデル名に変更
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
+
